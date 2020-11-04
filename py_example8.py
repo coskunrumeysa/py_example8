2d1540c541b87ca0678b75be1c888fc91a3fb9f3
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2020-11-04 14:49:15 +0300

    Add files via upload

---
+++ py_example8.py
@@ -0,0 +1,15 @@
+#Rümeysa Coşkun
+#Python çoklu variable atama örneği.
+#1.örnek
+x,y,z = "Limon","Portakal","Elma"
+
+print(x)
+print(y)
+print(z)
+
+#2.örnek
+x = y = z ="Karpuz"
+print(x)
+print(y)
+print(z)
+
